a44e505b63c999fe51111efcb95c02d125ba3932
TeeOnez <90501348+TeeOnez@users.noreply.github.com>
2024-02-17 14:28:34 +0500

    Uploding

---
+++ Getting Data From BackEnd & Del (Django)/Shop/tech/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+def Tiktok(request):
+    if request.method=="POST":
+        data = request.POST
+
+        name = data.get('name')
+        email = data.get('email')
+        Description = data.get('Description')
+        bootha = request.FILES.get('bootha')
+
+        tiktok.objects.create(
+            name = name,
+            email = email,
+            Description = Description,
+            bootha = bootha,
+            
+        )
+
+        return redirect('/')
+    
+    queryset = tiktok.objects.all()
+    context = {'Tiktokers': queryset}
+
+    return render(request, 'index.html', context)
+
+
+
+def del_tiktoker(request, id):
+    queryset = tiktok.objects.get(id=id)
+    queryset.delete()
+    return redirect('/')
---
+++ Getting Data From Frontend(Django)/Shop/tech/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+def Tiktok(request):
+    if request.method=="POST":
+        data = request.POST
+
+        name = data.get('name')
+        email = data.get('email')
+        Description = data.get('Description')
+        bootha = request.FILES.get('bootha')
+
+        tiktok.objects.create(
+            name = name,
+            email = email,
+            Description = Description,
+            bootha = bootha,
+            
+        )
+
+        return redirect('/')
+
+    return render(request, 'index.html')
